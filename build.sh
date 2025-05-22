f70c8e2173a8a4336318779797d858f086714578
ewelinaksiezniak <102612146+ewelinaksiezniak@users.noreply.github.com>
2025-05-22 09:12:35 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-NO_COLOR=1 docker build -t mysoft:latest -f Dockerfile .
+# NO_COLOR=1 docker build -t mysoft:latest -f Dockerfile .
+docker build -t mysoft:latest -f Dockerfile --progress=plain .
+
